8ab9939bd82ddacb81489c94b93353c2b341fbca
greyjacket <alex@syncurity.net>
2023-12-22 00:43:40 -0500

    Delegation

--- src/components/Ca.js
+++ src/components/Ca.js
@@ -93,45 +93,116 @@ const Ca = ({ ruleset, size }) => {
 
   export { Ca }; // Named export
 
-const caAutomaton = (ruleset, size, dim) => {
-  const generation = createMultiDimensionalMatrix(size, dim);
-    
-  const generateFirstVector = (size, random = false) => {
-    const createObject = () => ({
-        onState: 0,
-        color: [0, 0, 0],
-    });
+  const cellFactory = () => {
 
-    let firstVector = Array.from({ length: size }, (_) => createObject());
-
-    if (random) {
-        for (let i = 0; i < size; i++) {
-            const randomBit = Math.round(Math.random());
-            firstVector[i]['onState'] = randomBit;
-            firstVector[i] = generateColor(a[i]);
-        }
-    } else {
-        let halfway = Math.floor(size) / 2;
-        firstVector[halfway]['onState'] = 1;
-        firstVector[halfway] = generateColor(firstVector[halfway]);
+    /**
+     * Generates a multi-dimensional matrix of given size and dimensions.
+     * @param {number} size - The size of the matrix.
+     * @param {number} dim - The number of dimensions.
+     * @returns {Array} - The generated matrix.
+     */
+    const generateMultiDimensionalMatrix = (size, dim) => {
+      if(dim === 0) {
+          return 0;
+      } else {
+          let vector = new Array(size);
+          for(let i = 0; i < size; i++) {
+            vector[i] = createMultiDimensionalMatrix(size, dim - 1);
+          }
+          return vector;
+      }
     }
-    return firstVector;
+  
+    /**
+     * Generates the first vector of cells.
+     * @param {number} size - The size of the vector.
+     * @param {boolean} random - Whether to randomly initialize the vector.
+     * @returns {Array} - The generated vector.
+     */
+    const generateFirstVector = (size, random = false) => {
+      const createObject = () => ({
+          onState: 0,
+          color: [0, 0, 0],
+      });
+  
+      let firstVector = Array.from({ length: size }, (_) => createObject());
+  
+      if (random) {
+          for (let i = 0; i < size; i++) {
+              const randomBit = Math.round(Math.random());
+              firstVector[i]['onState'] = randomBit;
+              firstVector[i] = generateColor(a[i]);
+          }
+      } else {
+          let halfway = Math.floor(size) / 2;
+          firstVector[halfway]['onState'] = 1;
+          firstVector[halfway] = generateColor(firstVector[halfway]);
+      }
+      return firstVector;
+    };
+  
+    /**
+     * Generates the state of a cell based on a ruleset and the states of its neighbors.
+     * @param {Array} ruleset - The ruleset to use.
+     * @param {number} a - The state of the left neighbor.
+     * @param {number} b - The state of the cell itself.
+     * @param {number} c - The state of the right neighbor.
+     * @returns {number} - The new state of the cell.
+     */
+    const generateState = (ruleset, a, b, c) => {
+      let s = '' + a + b + c;
+      let index = parseInt(s, 2);
+      return ruleset[7 - index];
+    };
+  
+    /**
+     * Generates the next generation of a vector of cells based on a ruleset.
+     * @param {Array} vector - The current generation of cells.
+     * @param {Array} ruleset - The ruleset to use.
+     * @returns {Array} - The next generation of cells.
+     */
+    const generateVector = (vector, ruleset) => {
+      const createObject = () => ({
+          onState: 0,
+          color: [0, 0, 0],
+      });
+      size = vector.length;
+      if(vector.every(num => num === 0)){
+        generateFirstVector(size, random = false)
+      }
+      let nextgen = Array.from({ length: size }, (_) => createObject());
+  
+      for (let i = 1; i < size - 1; i++) {
+        let left = vector[i - 1]['onState'];
+        let me = vector[i]['onState'];
+        let right = vector[i + 1]['onState'];
+        let state = generateState(ruleset, left, me, right);
+        nextgen[i]['onState'] = state;
+      }
+      return nextgen;
+    };
+  
+    return {
+      generateMultiDimensionalMatrix,
+      generateFirstVector,
+      generateState,
+      generateVector,
+    };
   };
 
+const caKubernetes = (size, dim) => {
+  const generation = createMultiDimensionalMatrix(size, dim);
+  
   const set_ruleset = (ruleset) => {
     ruleset = ruleset;
   };
 
   const createRule = () => {
-      const randomArray = Array.from({ length: 8 }, () => Math.floor(Math.random() * 2));
-      ruleset = randomArray;
+    const randomArray = Array.from({ length: 8 }, () => Math.floor(Math.random() * 2));
+    ruleset = randomArray;
   };
 
-  const rules = (a, b, c) => {
-    let s = '' + a + b + c;
-    let index = parseInt(s, 2);
-    return ruleset[7 - index];
-  };
+
 
   const getRandomInteger = (min, max) => {
       return Math.floor(Math.random() * (max - min) + min);
@@ -145,57 +216,11 @@ const caAutomaton = (ruleset, size, dim) => {
     let rgb1 = getRandomInteger(0, 255);
     let rgb2 = getRandomInteger(0, 255);
     let rgb3 = getRandomInteger(0, 255);
-1
+
     cell['color'] = [rgb1, rgb2, rgb3];
     return cell;
   };
 
-  const generateVector = (vector) => {
-    const createObject = () => ({
-        onState: 0,
-        color: [0, 0, 0],
-    });
-    size = vector.length;
-    if(vector.every(num => num === 0)){
-      generateFirstVector(size, random = false)
-    }
-    let nextgen = Array.from({ length: size }, (_) => createObject());
-
-    for (let i = 1; i < size - 1; i++) {
-      let left = vector[i - 1]['onState'];
-      let me = vector[i]['onState'];
-      let right = vector[i + 1]['onState'];
-      let state = rules(left, me, right);
-      nextgen[i]['onState'] = state;
-    }
-    return nextgen;
-  };
-
-  return {
-    processFirstGen,
-    set_rules,
-    createRule,
-    rules,
-    getRandomInteger,
-    generateColor,
-    generate,
-  };
-};
-
-const caKubernetes = (ruleset, size, dim) => {
-  const generation = createMultiDimensionalMatrix(size, dim);
-  
-  function createMultiDimensionalMatrix(size, dim) {
-    if(dim === 0) {
-        return 0;
-    } else {
-        let arr = new Array(size);
-        for(let i = 0; i < size; i++) {
-            arr[i] = createMultiDimensionalMatrix(size, dim - 1);
-        }
-        return arr;
-    }
-  }
 };
 
 export { caFunction };
