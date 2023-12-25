5fccc44a4a338244e2d87be64f6d1028c11e06fe
Greyjacket <asch716@gmail.com>
2023-12-25 00:38:08 -0500

    Refactor

--- src/components/Ca.js
+++ src/components/Ca.js
@@ -1,99 +1,99 @@
-import React from 'react';
-import { useState } from "react";
+// import React from 'react';
+// import { useState } from "react";
 
-const Ca = ({ ruleset, size }) => {
-    const [generation, setGeneration] = useState([]);
+// const Ca = ({ ruleset, size }) => {
+//     const [generation, setGeneration] = useState([]);
   
-    const processFirstGen = (random = false) => {
-      const createObject = () => ({
-        onState: 0,
-        color: [0, 0, 0],
-      });
+//     const processFirstGen = (random = false) => {
+//       const createObject = () => ({
+//         onState: 0,
+//         color: [0, 0, 0],
+//       });
   
-      let a = Array.from({ length: size }, (_) => createObject());
+//       let a = Array.from({ length: size }, (_) => createObject());
   
-      if (random) {
-        for (let i = 0; i < size; i++) {
-          const randomBit = Math.round(Math.random());
-          a[i]['onState'] = randomBit;
-          a[i] = generateColor(a[i]);
-        }
-      } else {
-          let halfway = Math.floor(size) / 2;
-          a[halfway]['onState'] = 1;
-          a[halfway] = generateColor(a[halfway]);
-      }
+//       if (random) {
+//         for (let i = 0; i < size; i++) {
+//           const randomBit = Math.round(Math.random());
+//           a[i]['onState'] = randomBit;
+//           a[i] = generateColor(a[i]);
+//         }
+//       } else {
+//           let halfway = Math.floor(size) / 2;
+//           a[halfway]['onState'] = 1;
+//           a[halfway] = generateColor(a[halfway]);
+//       }
   
-      return a;
-    };
+//       return a;
+//     };
   
-    const set_rules = (ruleset) => {
-      ruleset = ruleset
-    };
+//     const set_rules = (ruleset) => {
+//       ruleset = ruleset
+//     };
   
-    const createRule = () => {
-      //   createRule(){
-        const randomArray = Array.from({ length: 8 }, () => Math.floor(Math.random() * 2));
-        ruleset = randomArray;
-    };
+//     const createRule = () => {
+//       //   createRule(){
+//         const randomArray = Array.from({ length: 8 }, () => Math.floor(Math.random() * 2));
+//         ruleset = randomArray;
+//     };
   
-    const rules = (a, b, c) => {
-      let s = '' + a + b + c;
-      let index = parseInt(s, 2);
-      return ruleset[7 - index];
-    };
+//     const rules = (a, b, c) => {
+//       let s = '' + a + b + c;
+//       let index = parseInt(s, 2);
+//       return ruleset[7 - index];
+//     };
   
-    const getRandomInteger = (min, max) => {
-      return Math.floor(Math.random() * (max - min) + min);
-    };
+//     const getRandomInteger = (min, max) => {
+//       return Math.floor(Math.random() * (max - min) + min);
+//     };
   
-    const generateColor = (cell) => {
-      let noiseLevel = 255;
-      let noiseScale = 0.1;
-      let nt = noiseScale;
+//     const generateColor = (cell) => {
+//       let noiseLevel = 255;
+//       let noiseScale = 0.1;
+//       let nt = noiseScale;
   
-      let rgb1 = getRandomInteger(0, 255);
-      let rgb2 = getRandomInteger(0, 255);
-      let rgb3 = getRandomInteger(0, 255);
+//       let rgb1 = getRandomInteger(0, 255);
+//       let rgb2 = getRandomInteger(0, 255);
+//       let rgb3 = getRandomInteger(0, 255);
   
-      cell['color'] = [rgb1, rgb2, rgb3];
-      return cell;
-    };
+//       cell['color'] = [rgb1, rgb2, rgb3];
+//       return cell;
+//     };
   
-    const generate = () => {
-      const createObject = () => ({
-        onState: 0,
-        color: [0, 0, 0],
-      });
+//     const generate = () => {
+//       const createObject = () => ({
+//         onState: 0,
+//         color: [0, 0, 0],
+//       });
   
-      let nextgen = Array.from({ length: size }, (_) => createObject());
+//       let nextgen = Array.from({ length: size }, (_) => createObject());
   
-      for (let i = 1; i < size - 1; i++) {
-        let left = generation[i - 1]['onState'];
-        let me = generation[i]['onState'];
-        let right = generation[i + 1]['onState'];
-        let state = rules(left, me, right);
-        nextgen[i]['onState'] = state;
+//       for (let i = 1; i < size - 1; i++) {
+//         let left = generation[i - 1]['onState'];
+//         let me = generation[i]['onState'];
+//         let right = generation[i + 1]['onState'];
+//         let state = rules(left, me, right);
+//         nextgen[i]['onState'] = state;
   
-        if (state == 1) {
-          nextgen[i] = generateColor(nextgen[i]);
-        }
+//         if (state == 1) {
+//           nextgen[i] = generateColor(nextgen[i]);
+//         }
         
-      }
-      setGeneration(nextgen);
-      return nextgen;
-    };
+//       }
+//       setGeneration(nextgen);
+//       return nextgen;
+//     };
   
-    return (
-      <div>
-        {/* Render your React components here */}
-      </div>
-    );
-  };
+//     return (
+//       <div>
+//         {/* Render your React components here */}
+//       </div>
+//     );
+//   };
 
-  export { Ca }; // Named export
+//   export { Ca }; // Named export
 
-const cellGenerator = () => {
+export const cellGenerator = () => {
 
   /**
    * Generates a multi-dimensional matrix of given size and dimensions.
@@ -188,7 +188,7 @@ const cellGenerator = () => {
   };
 };
 
-const cellGrapher = () => {
+export const cellGrapher = () => {
   const generateColor = (cell) => {
     let noiseLevel = 255;
     let noiseScale = 0.1;
@@ -205,7 +205,7 @@ const cellGrapher = () => {
   };
 }
 
-const cellKubernetes = (size, dim) => {
+export const cellKubernetes = (size, dim) => {
   let ruleset = [0,1,0,1,1,0,1,0]; //rule 90, sierpinski triangle
   const generation = cellGenerator.createMultiDimensionalMatrix(size, dim);
   
--- src/components/P5Canvas.js
+++ src/components/P5Canvas.js
@@ -1,18 +1,75 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import p5 from 'p5';
-import {Ca} from './Ca';
+//import {Ca} from './Ca';
+import { cellGenerator, cellGrapher, cellKubernetes } from './Ca';
 
 const P5Canvas = () => {
   // Function to define the sketch
   let ruleset = [0,1,0,1,1,0,1,0]; //rule 90, sierpinski triangle
+  const [generation, setGeneration] = useState([]);
+  const [isDrawing, setIsDrawing] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(600);
+  const [windowHeight, setwindowHeight] = useState(300);
+  let grid = [[]];
+  let cols = 0;
+  let rows = 0;
+  let resolution = 20;
+  let new_row = [];
+
+  function toggleDraw() {
+    setIsDrawing(!isDrawing);
+  }
+
+  function initialize() {
+    cols = Math.floor(windowWidth/resolution)
+    rows = Math.floor(windowHeight/resolution)
+    grid = cellGenerator.generateMultiDimensionalMatrix(rows, cols);
+    new_row = cellGenerator.generateFirstVector(cols, random=false);
+    grid.shift()
+    grid.push(new_row)
+    //ca.processFirstGen(randomBit=false);
+    //createComponents()
+  }
+  
+  function reinitialize() {
+    //let ca = new Ca(ruleset, cols, randomBit=true);
+    grid = cellGenerator.generateMultiDimensionalMatrix(rows, cols);    
+    new_row = cellGenerator.generateFirstVector(cols, random=false);
+    grid.shift()
+    grid.push(new_row)
+  }
+
+  function drawRow(row_number){
+    for (let j = 0; j < cols; j++) {
+      let x = j * resolution;
+      let y = row_number * resolution;
+      if (grid[row_number][j]['onState'] == 1) {
+        rgb1 = grid[row_number][j]['color'][0]
+        rgb2 = grid[row_number][j]['color'][1]
+        rgb3 = grid[row_number][j]['color'][2]
+        fill(rgb1, rgb2, rgb3);
+        stroke(0);
+        rect(x, y, resolution - 1, resolution - 1);
+      } 
+    }
+  };
+
+  function drawRows(){
+    newRow = ca.generate()
+    grid.shift()
+    grid.push(newRow)
+    for (let i = 0; i < rows; i++) {
+      drawRow(i);
+    }
+  }
 
   const sketch = (p) => {
     p.setup = () => {
-      let canvas = p.createCanvas(600, 300);
-      canvas.parent("sketch-container");      
+      let canvas = p.createCanvas(windowWidth, windowHeight);
       //let ca = Ca(ruleset, 370);
+      canvas.parent("sketch-container");      
     };
-
+    
     p.draw = () => {
       p.background(220);
       p.fill(0);
@@ -39,7 +96,6 @@ const P5Canvas = () => {
     </div>
   );
 };
-
 
 export default P5Canvas;
 
